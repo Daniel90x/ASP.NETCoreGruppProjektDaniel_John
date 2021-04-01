5df0156f46bbd2139a584deeb98d424d66eb8273
Daniel Persson <daniel.persson18@learnet.se>
2021-04-01 16:04:31 +0200

    Register basic fungerar,  både att göra nytt + logga in på ny användare

--- ASP.NETCoreGruppProjektDaniel_John/Pages/User/Register.cshtml.cs
+++ ASP.NETCoreGruppProjektDaniel_John/Pages/User/Register.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NETCoreGruppProjektDaniel_John.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,39 @@ namespace ASP.NETCoreGruppProjektDaniel_John.Pages.User
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<MyUser> _userManager;
+
+        public RegisterModel(UserManager<MyUser> userManager)
         {
+            _userManager = userManager;
+
+
+        }
+        [BindProperty]
+        public NewUserForm NewUser { get; set; }
+        public class NewUserForm
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+
+
+
+        public async Task<IActionResult> Onpost()
+        {
+            MyUser newUser = new MyUser()
+            {
+                UserName = NewUser.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, NewUser.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
