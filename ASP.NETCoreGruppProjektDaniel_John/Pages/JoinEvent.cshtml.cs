063dd99038591036f9016d6c6fab71528afc1dfd
Daniel Persson <daniel.persson18@learnet.se>
2021-04-02 11:13:29 +0200

    Scafolddat JoinEvent från Event med details templete

--- ASP.NETCoreGruppProjektDaniel_John/Pages/JoinEvent.cshtml.cs
+++ ASP.NETCoreGruppProjektDaniel_John/Pages/JoinEvent.cshtml.cs
@@ -1,16 +1,40 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP.NETCoreGruppProjektDaniel_John.Data;
+using ASP.NETCoreGruppProjektDaniel_John.Models;
 
 namespace ASP.NETCoreGruppProjektDaniel_John.Pages
 {
     public class JoinEventModel : PageModel
     {
-        public void OnGet()
+        private readonly ASP.NETCoreGruppProjektDaniel_John.Data.EventDbContext _context;
+
+        public JoinEventModel(ASP.NETCoreGruppProjektDaniel_John.Data.EventDbContext context)
         {
+            _context = context;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return Page();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
