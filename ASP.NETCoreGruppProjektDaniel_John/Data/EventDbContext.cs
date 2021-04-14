397a30b49c273e25805101c24fe09bf8c0f36ca6
Daniel Persson <daniel.persson18@learnet.se>
2021-04-14 14:29:13 +0200

    Fixat i HTML koden, försökt lägger till Organizer i Event och Organize Events

--- ASP.NETCoreGruppProjektDaniel_John/Data/EventDbContext.cs
+++ ASP.NETCoreGruppProjektDaniel_John/Data/EventDbContext.cs
@@ -20,43 +20,12 @@ namespace ASP.NETCoreGruppProjektDaniel_John.Data
         {
         }
 
-
-        /*private readonly ILogger<EventDbContext> _logger;
-        private readonly UserManager<MyUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-
-        public EventDbContext(
-            ILogger<EventDbContext> logger,
-            UserManager<MyUser> userManager,
-            RoleManager<IdentityRole> roleManager
-            )
-        {
-            _logger = logger;
-            _userManager = userManager;
-            _roleManager = roleManager;
-        }*/
-
-
         public DbSet<Event> Events { get; set; }
 
 
-        /*public void seed()
-        {
-            this.Database.EnsureCreated();
-
-            Events.AddRange(new List<Event>()
-            {
-                new Event(){Title = "Hejsan", Adress = "Här", Place = "Ny Place"}
-            });
-
-            this.SaveChanges();
-        } */
-
         public async Task SeedAsync(UserManager<MyUser> userManager, RoleManager<IdentityRole> roleManager, bool reset)
         {
-            //await this.Database.EnsureDeletedAsync();
-            //await this.Database.EnsureCreatedAsync();
+
             if (reset)
             {
                 await this.Database.EnsureDeletedAsync();
@@ -66,31 +35,27 @@ namespace ASP.NETCoreGruppProjektDaniel_John.Data
             {
                 return;
             }
-            Events.AddRange(new List<Event>()
-            {
-                new Event(){Title = "Hejsan", Adress = "Här", Place = "Ny Place", SpotsAvailable = 66},
-                new Event(){Title = "Hejsan1", Adress = "Här2", Place = "Ny Place3", SpotsAvailable = 17},
-                new Event(){Title = "Henrys hörna", Adress = "Strandgatan 7", Place = "True Heaven", SpotsAvailable = 183}
-            });
+           
 
             MyUser user = new MyUser()
             {
                 UserName = "admin",
                 Email = "admin@hotmail.com",
+                FirstName = "Admin",
             };
 
             MyUser user1 = new MyUser()
             {
                 UserName = "org",
                 Email = "organizer@hotmail.com",
-                
+                FirstName = "Organizer",
             };
 
             MyUser user2 = new MyUser()
             {
                 UserName = "Dude",
                 Email = "dude@hotmail.com",
-                
+                FirstName = "Dude",
             };
 
 
@@ -109,6 +74,16 @@ namespace ASP.NETCoreGruppProjektDaniel_John.Data
 
             await userManager.AddToRoleAsync(userRole, "admin");
             await userManager.AddToRoleAsync(userRole1, "organizer");
+
+
+
+            Events.AddRange(new List<Event>()
+            {
+                new Event(){Title = "Hejsan", Adress = "Här", Place = "Ny Place", SpotsAvailable = 66, Organizer = userRole1},
+                new Event(){Title = "Hejsan1", Adress = "Här2", Place = "Ny Place3", SpotsAvailable = 17, Organizer = userRole1},
+                new Event(){Title = "Henrys hörna", Adress = "Strandgatan 7", Place = "True Heaven", SpotsAvailable = 183, Organizer = userRole1}
+            });
+
 
 
             await SaveChangesAsync();
